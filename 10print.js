35d59539c59676e8f7c47a2474fefea3860361ba
Sam Schwartz <sschwartz222@gmail.com>
2020-05-03 04:45:10 -0500

    super basic remix seeing if source control works added different characters every other line is a reversed version of the previous line

--- 10print.js
+++ 10print.js
@@ -2,19 +2,27 @@
   one possible implemenation of the 10print.org algorithm written in JavaScript
 */
 
-const w = process.stdout.columns
+const w = process.stdout.columns/3
 
-function draw () {
-  setTimeout(draw, 1000)
-  let output = ''
-  for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
-    }
+let output = ''
+
+function draw (counter) {
+  if (counter >= 10) return 1
+  else if (counter % 2 === 0){
+    output = ''
+    for (let i = 0; i < w; i++) {
+      if (Math.random() > 0.5) {
+        output += '╲┋╱'
+      } else {
+        output += '╱┋╲'
+      }
+    } 
+  } 
+  else {
+      output = output.split('').reverse().join('');      
   }
-  console.log(output)
+    console.log(output)
+    setTimeout(draw, 5, counter+1)
 }
 
-draw()
+draw(0)
